09062ab456fe51540135c34db44190d6324c8d4d
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-03-19 15:30:52 +0600

    Initial Commit

---
+++ ArmanCard/ArmanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmanCardApp.swift
+//  ArmanCard
+//
+//  Created by Arman Myrzakanurov on 19.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ArmanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
